995e15ac18983d5e902309e6d5eac85b9e2eb566
0GuiSobral7 <guilherme.martorani@gmail.com>
2024-04-29 22:33:11 -0300

    Aula com Projeto mobile

---
+++ Projeto-Basico/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={estilos.texto}>Oi tudo bem?</Text>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
